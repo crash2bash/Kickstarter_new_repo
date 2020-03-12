f9f934ce6232186fc4d5e5fc864f5c052dbceefd
crash2bash <sachagurov@gmail.com>
2020-03-12 09:58:32 +0200

    add caption line and text

--- src/scripts/technologies.js
+++ src/scripts/technologies.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const buttonTechnologies = document.querySelectorAll('.technology__details');
+// const buttonTechnologies = document.querySelectorAll('.technology__details');
+const button = document.querySelector('.technology__details--seven');
+const captionLine = document.querySelector('.technology__caption-line');
+const captionText = document.querySelector('.technology__caption-text');
 
-buttonTechnologies.forEach(button => {
-  button.addEventListener('click', function() {
-    button.classList.add('technology__details--active');
-  });
+// buttonTechnologies.forEach(button => {
+//   button.addEventListener('click', function() {
+//     button.classList.add('technology__details--active');
+//   });
+// });
+
+button.addEventListener('click', function() {
+  captionLine.classList.toggle('show');
+  captionText.classList.toggle('show');
 });
